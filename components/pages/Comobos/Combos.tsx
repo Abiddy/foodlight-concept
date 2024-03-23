62aabf5150b5f125d861816e3faf4cf9d0e967a7
Abiddy <abidinouman@gmail.com>
2024-03-23 06:59:55 -0700

    reverting combo for now

--- components/pages/Comobos/Combos.tsx
+++ components/pages/Comobos/Combos.tsx
@@ -3,11 +3,9 @@ import { League_Spartan } from 'next/font/google';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Database } from '../../../types/supabase';
 import ItemCard from './Item-Cards';
-import { IonBadge, IonButton, IonChip, IonIcon, IonInput, IonRange } from '@ionic/react';
+import { IonBadge, IonChip, IonIcon } from '@ionic/react';
 import Store from '../../../store';
 import { setMenuItems } from '../../../store/actions';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 import { caretBackOutline, caretForwardOutline, cashOutline } from 'ionicons/icons';
 import { league_spartan_bold } from '../BuildOrder';
 
@@ -94,8 +92,6 @@ const ComboCard = ({ userId, combination }) => {
     return totalPrice <= budget;
   });
 
-  console.log({filteredCombinations})
-
   const decreaseBudget = () => {
     const newBudget = budget - 5;
     setBudget(newBudget);
@@ -138,75 +134,7 @@ const ComboCard = ({ userId, combination }) => {
         {filteredCombinations.map((combo, index) => {
          const nonEmptyHeaders = Object.entries(combo.item_ids).filter(([header, items]) => items.length > 0);
           return (
-        //     <Card key={index}  className="mt-15 mb-5 rounded-xl relative p-5" style={{ boxShadow: 'none', border: '0.8px solid'}}>
-        // <div className="right-0 mb-2 flex-shrink-0 ml-auto flex items-center gap-2">
-        //   <IonBadge color="warning" className={`${league_spartan.className} text-l mt-1`} style={{ padding: '10px'}}>Total: ${combo.reduce((acc, item) => acc + item.item_price, 0).toFixed(2)}</IonBadge>
-        //   </div>
-        //   {nonEmptyHeaders.map((item, index2) => (
-        //     <Card key={index2} className="w-full max-w-[48rem] flex-row mb-4" >
-        //       <CardHeader shadow={false} floated={false} className="m-0 w-2/5 shrink-0 rounded-r-none">
-        //         <img
-        //           src={item.item_image_url || 'default-image-url'}
-        //           alt="card-image"
-        //           className="h-full w-full object-cover"
-        //         />
-        //         <div className="absolute top-2 left-2">
-        //             <IonIcon
-        //             icon={refresh}
-        //             onClick={() => {
-        //               setIsOpen(true);
-        //               setindexToChange([index, index2]);
-        //               setComboPrice([combo, item.item_price]);
-        //             }}
-        //             shape='round'
-        //             className="ion-margin-end"
-        //             style={{
-        //               color: 'black',
-        //               fontSize: '1.3rem',
-        //               border: 'solid white',
-        //               backgroundColor: 'white',
-        //               boxShadow: '0px 2px 4px rgba(0, 0, 0, 5)',
-        //               padding: '0px', 
-        //               borderRadius: '50%',
-
-        //             }}
-        //           />
-        //         </div>
-        //       </CardHeader>
-        //       <CardBody className='p-2'>
-        //       <Typography style={{ fontWeight: 'bold' ,color: '#333',fontSize: '1rem'}} className={league_spartan.className}>
-        //           {item.item_name}
-        //       </Typography>
-        //       <Typography style={{ fontSize: '0.8rem' }} className={league_spartan.className}>
-        //           ${item.item_price}
-        //       </Typography>
-        //       <div className="relative flex items-center justify-between">
-        //         <Typography color="gray" variant="p" className={league_spartan_light.className} style={{ color: '#333'}}>
-        //           {truncateText(item.item_description, 5)}
-        //         </Typography>
-        //       </div>
-        //       </CardBody>
-        //   </Card>
-        //   ))}
-        //     <Button
-        //       onClick={() => addToCart(item)}
-        //       className="flex items-center justify-between"
-        //       style={{
-        //         color: 'white',
-        //         background: '#2D3142',
-        //         borderRadius: '9999px',
-        //         padding: '10px 12px', 
-        //         fontSize: '13.5px', 
-        //         fontWeight: 'bold',
-        //         boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.4)', 
-        //       }}
-        //     >
-        //       Add Combo
-        //       <IonIcon icon={arrowForward}/>
-        //     </Button>
-        // </Card>
-
-
+            
             <div key={index} style={{ marginBottom: '50px' }} className="p-2 text-center mt-15 mb-15  border border-gray-300 shadow-md rounded-xl">
               <div className="flex justify-between p-3">
                 <h1 className={league_spartan.className}>Combo #{combo.index[0]}</h1>
